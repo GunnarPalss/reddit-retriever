ff8e86a4bfd2e02a1abd350df9dce76b78dd4d74
Gunnar Pálsson <gup19@outlook.com>
2025-01-19 13:33:13 +0000

    Cross-Origin stuff

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
-// /pages/api/auth/[...nextauth].js
-import NextAuth from "next-auth";
-import RedditProvider from "next-auth/providers/reddit"; // Corrected provider import
+import NextAuth from 'next-auth';
+import RedditProvider from 'next-auth/providers/reddit';
 
 export default NextAuth({
   providers: [
@@ -8,11 +7,38 @@ export default NextAuth({
       clientId: process.env.REDDIT_CLIENT_ID,
       clientSecret: process.env.REDDIT_CLIENT_SECRET,
       authorizationUrl: 'https://www.reddit.com/api/v1/authorize',
-      scope: 'read',  // Permissions
+      scope: 'read',
     }),
   ],
   secret: process.env.JWT_SECRET,
   session: {
     jwt: true,
+  },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,  // Use a secure cookie name
+      options: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',  // Only set secure cookies in production
+        sameSite: 'lax',  // To allow cross-site cookies
+      },
+    },
+  },
+  pages: {
+    signIn: '/auth/signin',
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.accessToken = token.accessToken;
+      }
+      return session;
+    },
   },
 });
