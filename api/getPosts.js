5c4de5a952fed4e9036b546d640be23d79974391
Gunnar Pálsson <gup19@outlook.com>
2025-01-19 00:35:57 +0000

    axios comp prob 2

--- api/getPosts.js
+++ api/getPosts.js
@@ -1,4 +1,4 @@
-import axios from '/var/task/node_modules/axios';
+const axios = require('axios')
 
 export default async function handler(req, res) {
   const { subreddit = 'technology' } = req.query; // Default subreddit: 'technology'
