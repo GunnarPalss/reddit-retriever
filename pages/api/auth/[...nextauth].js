2f6dab9d10c883307b3eaa227bfefb1c2c835e8c
Gunnar Pálsson <gup19@outlook.com>
2025-01-19 14:00:47 +0000

    simplify nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,26 +8,9 @@ export default NextAuth({
       clientSecret: process.env.REDDIT_CLIENT_SECRET,
       authorizationUrl: 'https://www.reddit.com/api/v1/authorize',
       scope: 'read',
-      redirectUri: 'https://reddit-retriever.vercel.app/api/auth/callback/reddit'
     }),
   ],
   secret: process.env.JWT_SECRET,
-  session: {
-    jwt: true,
-  },
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,  // Use a secure cookie name
-      options: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',  // Only set secure cookies in production
-        sameSite: 'lax',  // To allow cross-site cookies
-      },
-    },
-  },
-  pages: {
-    signIn: '/auth/signin',
-  },
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
