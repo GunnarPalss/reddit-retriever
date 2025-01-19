eabdca2fceb635121ad3483d49fb719514cf161f
Gunnar Pálsson <gup19@outlook.com>
2025-01-19 17:10:47 +0000

    no redditAuth + simplify getPosts

--- pages/api/getPosts.js
+++ pages/api/getPosts.js
@@ -1,25 +1,16 @@
 import axios from 'axios';
-import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
-  const session = await getSession({ req });
+    const { subreddit } = req.query;
+    const { data } = await axios.get(`https://www.reddit.com/r/${subreddit}.json`);
 
-  const { subreddit } = req.query;
-  const accessToken = session.user.accessToken;
-  console.log("komst í getPosts")
-  const response = await axios.get(`https://oauth.reddit.com/r/${subreddit}.json`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+    const posts = data.data.children.map((post) => ({
+      title: post.data.title,
+      url: post.data.url,
+      author: post.data.author,
+      score: post.data.score,
+    }));
 
-  const posts = response.data.data.children.map((post) => ({
-    title: post.data.title,
-    url: post.data.url,
-    author: post.data.author,
-    score: post.data.score,
-  }));
-
-  res.status(200).json(posts);
+    res.status(200).json(posts);
 
 }
